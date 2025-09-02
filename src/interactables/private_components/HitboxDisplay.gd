bddbe33a9b0a3b131026b74abe0c85992b6421f1
enderprism <endxrprism@gmail.com>
2025-09-02 10:09:36 +0200

    config-driven hitbox display color

--- src/interactables/private_components/HitboxDisplay.gd
+++ src/interactables/private_components/HitboxDisplay.gd
@@ -2,7 +2,8 @@ extends Node2D
 class_name HitboxDisplay
 
 @export var hitbox: CollisionShape2D
-@export var color: Color
+@export var color_config_key: String
+@export var fill_alpha_config_key: String
 
 
 func _process(_delta: float) -> void:
@@ -10,9 +11,13 @@ func _process(_delta: float) -> void:
 
 
 func _draw() -> void:
+	var color: Color = Config.config.get(color_config_key)
+	var fill_color = color
+	fill_color.a = Config.config.get(fill_alpha_config_key)
 	match hitbox.shape:
 		var rectangle when hitbox.shape is RectangleShape2D:
 			draw_rect(Rect2(-rectangle.size / 2, rectangle.size), color, false, 4.0)
+			draw_rect(Rect2(-rectangle.size / 2, rectangle.size), fill_color)
 		var line when hitbox.shape is SegmentShape2D:
 			draw_line(line.a, line.b, color, 4.0)
 
