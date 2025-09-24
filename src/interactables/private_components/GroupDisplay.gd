0de2e8a08155202ed6c52200f367d4c0b2cb613a
enderprism <endxrprism@gmail.com>
2025-09-24 21:08:33 +0200

    fix: group display is empty when opening level

--- src/interactables/private_components/GroupDisplay.gd
+++ src/interactables/private_components/GroupDisplay.gd
@@ -1,8 +1,17 @@
 extends Control
 
+# HACK: Workaround for the label text not being serialized
+@export_storage var displayed_group: String
+
+
+func _ready() -> void:
+	var restore_displayed_group := func(): $Label.text = displayed_group
+	restore_displayed_group.call_deferred()
+
 
 func _on_target_group_component_changed(target_group: String) -> void:
-	$Label.text = target_group.trim_prefix(GroupEditor.GROUP_PREFIX)
+	displayed_group = target_group.trim_prefix(GroupEditor.GROUP_PREFIX)
+	$Label.text = displayed_group
 	var update_width := func(): $Label.position.x = -$Label.size.x/2
 	update_width.call_deferred()
 
