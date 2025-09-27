f77543805f028938764a99229b3a5010741c55a9
enderprism <endxrprism@gmail.com>
2025-09-27 21:09:44 +0200

    fix: editor manual doesn't auto-hide when starting playtest

--- src/EditorScene.gd
+++ src/EditorScene.gd
@@ -114,6 +114,7 @@ func _on_playtest_pressed() -> void:
 		%EditorModes.hide()
 		%SidePanel.hide()
 		%LevelSettings.hide()
+		%EditorManual.hide()
 		%EditorViewport.mouse_filter = MOUSE_FILTER_STOP
 		$GameScene/Player.process_mode = Node.PROCESS_MODE_INHERIT
 		$GameScene/PercentageLayer.show()
