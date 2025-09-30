6a5fb5ac410725455848c7cbc46f310c01c1bbcb
PushSL <peter@anthonyfok.org>
2025-09-30 11:55:03 -0600

    Fixed Sprite hiding not working

--- src/attributes/HideAttribute/HideSpriteAttribute.gd
+++ src/attributes/HideAttribute/HideSpriteAttribute.gd
@@ -4,11 +4,10 @@ class_name HideSpriteAttribute
 
 func _ready() -> void:
 	for node in parent:
-		print(node.get_class())
-		#if node and node.get_class() == "Sprite2D" or node.name.contains("Sprite") and not node.name == "NinePatchSprite2DAbsoluteSize" and not node.get_name() == "HideSpriteAttribute":
-			#node.hide()
-			#continue
-		#parent.remove_at(parent.find(node))
+		if node and node.get_class() == "Sprite2D" or node.name.contains("Sprite") and not node.name == "NinePatchSprite2DAbsoluteSize" and not node.get_name() == "HideSpriteAttribute":
+			node.hide()
+			continue
+
 
 
 func _exit_tree() -> void:
