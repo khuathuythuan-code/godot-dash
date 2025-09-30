49a2ceff476b381e50d1c7da562f07363d713dd0
PushSL <peter@anthonyfok.org>
2025-09-30 11:36:36 -0600

    Fixed a crash

--- src/attributes/HideAttribute/HideSpriteAttribute.gd
+++ src/attributes/HideAttribute/HideSpriteAttribute.gd
@@ -1,13 +1,14 @@
 extends HideAttribute
 class_name HideSpriteAttribute
-@onready var parent := get_parent()
+@onready var parent := get_parent().get_children()
 
 func _ready() -> void:
 	for node in parent:
-		if node and node.get_class() == "Sprite2D" or node.name.contains("Sprite") and not node.name == "NinePatchSprite2DAbsoluteSize" and not node.get_name() == "HideSpriteAttribute":
-			node.hide()
-			continue
-		parent.remove_at(parent.find(node))
+		print(node.get_class())
+		#if node and node.get_class() == "Sprite2D" or node.name.contains("Sprite") and not node.name == "NinePatchSprite2DAbsoluteSize" and not node.get_name() == "HideSpriteAttribute":
+			#node.hide()
+			#continue
+		#parent.remove_at(parent.find(node))
 
 
 func _exit_tree() -> void:
