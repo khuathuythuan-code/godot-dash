95b405d1770128de8c4ffc89dad98ba60114e317
PushSL <peter@anthonyfok.org>
2025-10-03 20:15:32 -0600

    Optimized HSVWatchers

--- src/HSVWatcher.gd
+++ src/HSVWatcher.gd
@@ -5,6 +5,7 @@ class_name HSVWatcher
 @export_storage var hsv_shift: Array[float]
 @export_storage var strength: float = 1.0
 @export_storage var alpha: float = 1.0
+var last_modulate
 
 
 func _ready() -> void:
@@ -21,9 +22,12 @@ func _process(_delta: float) -> void:
 	if has_node("SelectionHighlight"):
 		get_parent().modulate = modulate
 		return
+	if last_modulate == modulate:
+		return
 	var shifted_modulate := modulate
 	shifted_modulate.h = fmod(shifted_modulate.h + hsv_shift[0], 1.0)
 	shifted_modulate.s += hsv_shift[1]
 	shifted_modulate.v += hsv_shift[2]
 	get_parent().modulate = shifted_modulate * strength
 	get_parent().modulate.a = alpha * modulate.a
+	modulate = get_parent().modulate
