f0a98f11b0f52b7d3866dfffe7be2811a5f51b98
PushSL <peter@anthonyfok.org>
2025-10-06 20:37:37 -0600

    Added performance settings

---
+++ scenes/particle_emitter_presets/SpeedPortalParticles.gd
@@ -0,0 +1,20 @@
+extends GPUParticles2D
+
+
+func _ready() -> void:
+	if Config.config.hide_speedportal_particles:
+		if Config.config.hide_particles_editor_only:
+			if Editor.in_editor: # IDK HOW THIS WORKS
+				call_deferred("hide")
+		else:
+			call_deferred("hide")
+			set_script(null)
+			return
+	if not Config.config.preprocess_speedportal_particles:
+		if Config.config.preprocess_particles_editor_only:
+			if Editor.in_editor: # IDK HOW THIS WORKS
+				preprocess = 0.0
+		else:
+			preprocess = 0.0
+			set_script(null)
+			return
--- src/GameScene.gd
+++ src/GameScene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name GameScene
 
+
 func _ready() -> void:
 	LevelManager.game_scene = self
 	Engine.time_scale = 1
