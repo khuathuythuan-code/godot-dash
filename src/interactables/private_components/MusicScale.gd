151f18d099d406948d5659909add8061f69fcdd1
PushSL <peter@anthonyfok.org>
2025-10-06 20:52:20 -0600

    Made music scale calculations perform on the sub-thread

--- src/interactables/private_components/MusicScale.gd
+++ src/interactables/private_components/MusicScale.gd
@@ -5,5 +5,9 @@ class_name MusicScale
 @onready var initial_scale: Vector2 = parent.scale
 @onready var music_scale: float = LevelManager.current_level.music_scale
 
+func _ready() -> void:
+	process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
+
 func _process(delta):
-	parent.scale = parent.scale.lerp(initial_scale * (music_scale), 1-exp(-delta * 12))
+	parent.set_thread_safe("scale", parent.scale.lerp(initial_scale * (music_scale), 1-exp(-delta * 12)))
+	#parent.scale = parent.scale.lerp(initial_scale * (music_scale), 1-exp(-delta * 12))
