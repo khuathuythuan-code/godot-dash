7003cfc623dbafd1a820efda33e81fab962d57c1
PushSL <peter@anthonyfok.org>
2025-10-08 16:11:38 -0600

    fixed a crash when using culling and switching levels

--- src/NodeFeeder.gd
+++ src/NodeFeeder.gd
@@ -2,6 +2,8 @@ extends Node
 @onready var parent: Node = get_parent()
 
 func _feed() -> void:
+	if parent.parent == null:
+		return
 	var objects: Array[Node] = parent.parent.get_children()
 	objects.reverse()
 	parent.call_deferred("cull", objects)
