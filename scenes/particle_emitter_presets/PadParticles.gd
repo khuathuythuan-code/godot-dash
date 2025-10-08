21c05e3f98ce2be08c5ac84ec4d511d07ce6c29c
PushSL <peter@anthonyfok.org>
2025-10-07 22:20:53 -0600

    resolved particle script comment

--- scenes/particle_emitter_presets/PadParticles.gd
+++ scenes/particle_emitter_presets/PadParticles.gd
@@ -4,16 +4,20 @@ extends GPUParticles2D
 func _ready() -> void:
 	if Config.config.hide_pad_particles:
 		if Config.config.hide_particles_editor_only:
-			if Editor.in_editor: # IDK HOW THIS WORKS
+			if Editor.in_editor:
 				call_deferred("hide")
+			set_script(null)
+			return
 		else:
 			call_deferred("hide")
 			set_script(null)
 			return
 	if not Config.config.preprocess_pad_particles:
 		if Config.config.preprocess_particles_editor_only:
-			if Editor.in_editor: # IDK HOW THIS WORKS
+			if Editor.in_editor:
 				preprocess = 0.0
+			set_script(null)
+			return
 		else:
 			preprocess = 0.0
 			set_script(null)
