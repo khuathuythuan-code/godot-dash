617bf6c7000853d8e5586ea312d94278f61f17ed
PullSL <paul@anthonyfok.org>
2025-10-09 12:49:20 -0600

    add foldablegroup to transform panel 	modified:   scenes/EditorScene.tscn

--- src/editor/editor_panels/TransformEditor.gd
+++ src/editor/editor_panels/TransformEditor.gd
@@ -6,6 +6,7 @@ extends VBoxContainer
 @export var rotation_node: FloatProperty
 @onready var parent: Node = get_parent()
 var current_selection: Array[Node2D]
+var selection_size: int
 var first_object: Node2D
 
 
@@ -16,14 +17,28 @@ func _on_edit_handler_selection_changed(selection: Array[Node2D]) -> void:
 		return
 	parent.show()
 	first_object = current_selection.get(0)
-	_process(0)
-
+	selection_size = selection.size()
 
 
 func _process(_delta: float) -> void:
 	if current_selection.is_empty():
 		return
 	z_index_node.set_value_no_signal(float(first_object.z_index))
-	scale_node.set_value_no_signal(first_object.scale)
-	position_node.set_value_no_signal((first_object.position / LevelManager.CELL_SIZE + Vector2(0, 0.5)) * Vector2(1, -1))
-	rotation_node.set_value_no_signal(first_object.rotation_degrees)
+	if selection_size == 1:
+		scale_node.set_value_no_signal(first_object.scale)
+		position_node.set_value_no_signal((first_object.position / LevelManager.CELL_SIZE + Vector2(0, 0.5)) * Vector2(1, -1))
+		rotation_node.set_value_no_signal(first_object.rotation_degrees)
+		return
+	var scales: Array[Vector2]
+	current_selection.map(func(object): scales.append(object.scale))
+	var same_scale: bool # always 1 when not all the same
+	var first_value = scales[0]
+	for value in scales:
+		if value != first_value:
+			same_scale = false
+			break
+	var positions: Array[Vector2]
+	current_selection.map(func(object): scales.append(object.scale))
+	var average_position: Vector2
+	
+	var same_rotation: bool # always 0 when not all the same
