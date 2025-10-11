110bb180c2eb50ba6b84aa716449187526a6becb
PushSL <peter@anthonyfok.org>
2025-10-10 21:32:34 -0600

    hide mobile features on pc

--- src/editor/SelectOptions.gd
+++ src/editor/SelectOptions.gd
@@ -2,5 +2,9 @@ extends MarginContainer
 
 @onready var EditorCamera := $"../../../../../../../EditorCamera"
 
+func _ready() -> void:
+	if not DisplayServer.is_touchscreen_available():
+		queue_free()
+
 func _on_swipe_toggled(toggled_on: bool) -> void:
 	EditorCamera.swipe = toggled_on
