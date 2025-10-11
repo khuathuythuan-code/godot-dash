2b062e23a11c943f37da0476ab95f5c817d0af1a
enderprism <endxrprism@gmail.com>
2025-10-11 20:21:09 +0200

    fix: rotation pin lags behind rotation gizmo

--- src/interactables/public_components/TextureRotationPinComponent.gd
+++ src/interactables/public_components/TextureRotationPinComponent.gd
@@ -8,15 +8,13 @@ class_name TextureRotationPinComponent
 
 
 func _process(_delta: float) -> void:
-	if pinned == null:
+	if not pinned:
 		return
 	if not Engine.is_editor_hint() and LevelManager.player_camera != null:
 		if pin_to_gameplay_rotation:
-			pinned.global_rotation = LevelManager.player.gameplay_rotation
+			pinned.set_deferred(&"global_rotation", LevelManager.player.gameplay_rotation)
 		else:
-			pinned.global_rotation = LevelManager.player_camera.rotation
+			pinned.set_deferred(&"global_rotation", LevelManager.player_camera.rotation)
 	else:
-		pinned.global_rotation = 0.0
-	pinned.global_scale.x = abs(parent.scale.x)
-	pinned.global_scale.y = abs(parent.scale.y)
-	pinned.global_scale *= sprite_scale
+		pinned.set_deferred(&"global_rotation", 0.0)
+	pinned.set_deferred(&"global_scale", parent.scale.abs() * sprite_scale)
