414d1c4db507cfc9c214d9aaff1951778d5ce258
enderprism <endxrprism@gmail.com>
2025-10-11 19:48:09 +0200

    fix: transform panel sometimes doesn't hide; swap groups and transform panels

--- src/editor/SidePanelManager.gd
+++ src/editor/SidePanelManager.gd
@@ -4,6 +4,9 @@ extends Node
 @export var side_panel: PanelContainer
 @export var object_name: LineEdit
 
+@export_group("Transform")
+@export var transform_section: FoldableContainer
+
 @export_group("Groups")
 @export var group_section: FoldableContainer
 @export var group_editor: GroupEditor
@@ -41,6 +44,8 @@ func _on_edit_handler_selection_changed(selection: Array[Node2D]) -> void:
 		object_name.editable = false
 		group_parent.set_value_no_signal(false)
 		group_parent.set_input_state(false)
+
+	transform_section.visible = not selection.is_empty()
 	
 	group_section.visible = not selection.is_empty()
 	group_editor.selected_objects = selection
