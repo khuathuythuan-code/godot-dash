aeb6ca9d59d3460e5c137ee7e44f8bc0b9b25d0b
enderprism <endxrprism@gmail.com>
2025-10-21 11:43:56 +0200

    formatting

--- src/gui/properties/types/EnumProperty.gd
+++ src/gui/properties/types/EnumProperty.gd
@@ -11,6 +11,7 @@ signal value_changed(value: String)
 
 var input: OptionButton
 
+
 func _ready() -> void:
 	label = NodeUtils.get_node_or_add(self, "Label", Label, NodeUtils.INTERNAL)
 	input = NodeUtils.get_node_or_add(self, "Input", OptionButton, NodeUtils.INTERNAL)
@@ -21,20 +22,25 @@ func _ready() -> void:
 		.get_node_or_add(self, "PropertyReset", PropertyReset, NodeUtils.INTERNAL) \
 		.set_input(input)
 
+
 func set_value(new_value: int) -> void:
 	_value = new_value
 	input.selected = new_value
 	value_changed.emit(new_value)
 
+
 func set_value_no_signal(new_value: int) -> void:
 	_value = new_value
 	input.selected = new_value
 
+
 func get_value() -> int:
 	return input.selected
 
+
 func reset() -> void:
 	set_value(default)
+
 
 func refresh() -> void:
 	label.text = name
@@ -45,6 +51,7 @@ func refresh() -> void:
 		input.add_item(field if Config.config.enable_easter_eggs else field.trim_prefix("Trans "))
 	if Engine.is_editor_hint():
 		reset()
+
 
 func set_input_state(enabled: bool) -> void:
 	input.disabled = not enabled
