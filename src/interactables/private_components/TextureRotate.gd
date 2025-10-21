7e162a3b17ef15e27be90bbc72f685fb77e34885
enderprism <endxrprism@gmail.com>
2025-10-21 11:30:35 +0200

    make TextureRotate a public component

--- src/interactables/private_components/TextureRotate.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name TextureRotate
-
-@export var sprite: Sprite2D
-@export_range(-360.0, 360.0, 0.01, "or_greater", "or_less") var rotation_rate_degrees: float
-
-
-func _process(delta: float) -> void:
-	sprite.rotation_degrees += rotation_rate_degrees * delta
