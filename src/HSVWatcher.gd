bbcb22a7e93c798470a3d74147bff809fa708c29
enderprism <endxrprism@gmail.com>
2025-10-28 11:30:47 +0100

    fix: colorchannelwatchers overwrite hsv strength and alpha

--- src/HSVWatcher.gd
+++ src/HSVWatcher.gd
@@ -10,6 +10,9 @@ enum SelectionHighlight {
 @export_storage var hsv_shift: Array[float]
 @export_storage var strength: float = 1.0
 @export_storage var alpha: float = 1.0
+# Set by ColorChannelWatchers
+@export_storage var base_strength: float = 1.0
+@export_storage var base_alpha: float = 1.0
 
 var selection_highlight: SelectionHighlight
 
@@ -40,19 +43,21 @@ func serialize() -> Dictionary:
 func use_data(data: Dictionary) -> void:
 	hsv_shift.assign(data.hsv_shift)
 	strength = data.strength
+	print_debug(strength)
 	alpha = data.alpha
 
 
 func _update_color() -> void:
 	var shifted_modulate: Color = modulate
-	# FIXME: This field won't change and I can't figure out why
-	shifted_modulate.h = fposmod(shifted_modulate.h + hsv_shift[0], 1.0)
+	# FIXME: This field won't change and I can't figure out why.
+	#        It works fine when the object has a color channel…
+	shifted_modulate.h += hsv_shift[0]
 	shifted_modulate.s += hsv_shift[1]
 	shifted_modulate.v += hsv_shift[2]
 	match selection_highlight:
 		SelectionHighlight.NONE:
-			parent.modulate = shifted_modulate * strength
-			parent.modulate.a = alpha * modulate.a
+			parent.modulate = shifted_modulate * strength * base_strength
+			parent.modulate.a = modulate.a * alpha * base_alpha
 		SelectionHighlight.NORMAL:
 			parent.modulate = Color.GREEN
 		SelectionHighlight.DUPLICATE:
