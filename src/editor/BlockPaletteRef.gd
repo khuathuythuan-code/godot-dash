6cdb7e84356e4636c16dcaefd8e1f2a25077a346
enderprism <endxrprism@gmail.com>
2025-10-28 14:35:49 +0100

    remove unused selection collider type

--- src/editor/BlockPaletteRef.gd
+++ src/editor/BlockPaletteRef.gd
@@ -7,13 +7,7 @@ class_name BlockPaletteRef
 		notify_property_list_changed()
 @export var id: int
 @export var object: PackedScene
-@export var trigger_script: Script
 
-func _validate_property(property: Dictionary) -> void:
-	if property.name == "object" and type == EditorSelectionCollider.Type.TRIGGER:
-		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name == "trigger_script" and type != EditorSelectionCollider.Type.TRIGGER:
-		property.usage = PROPERTY_USAGE_NO_EDITOR
 
 func _ready() -> void:
 	if type == EditorSelectionCollider.Type.INTERACTABLE and not Engine.is_editor_hint():
