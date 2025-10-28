6e50e32fe38c964aee5325acb15c88f85ec97461
enderprism <endxrprism@gmail.com>
2025-10-28 11:40:56 +0100

    fix: multi-selection copies hsv to all selected objects

--- src/editor/HSVHandler.gd
+++ src/editor/HSVHandler.gd
@@ -40,13 +40,13 @@ func _on_edit_handler_selection_changed(selection: Array[Node2D]) -> void:
 	var objects_with_hsv_watcher: Array[Node2D] = selection.filter(_has_hsv_watcher)
 	if objects_with_hsv_watcher.is_empty():
 		for property in [hue, saturation, value]:
-			property.set_value(0.0)
-		strength.set_value(1.0)
-		alpha.set_value(1.0)
+			property.set_value_no_signal(0.0)
+		strength.set_value_no_signal(1.0)
+		alpha.set_value_no_signal(1.0)
 	else:
-		var last_hsv_watcher = objects_with_hsv_watcher[-1].get_node("HSVWatcher")
-		hue.set_value(last_hsv_watcher.hsv_shift[0])
-		saturation.set_value(last_hsv_watcher.hsv_shift[1])
-		value.set_value(last_hsv_watcher.hsv_shift[2])
-		strength.set_value(last_hsv_watcher.strength)
-		alpha.set_value(last_hsv_watcher.alpha)
+		var last_hsv_watcher = BaseDetailHandler.use_hsv_watcher(objects_with_hsv_watcher.back())
+		hue.set_value_no_signal(last_hsv_watcher.hsv_shift[0])
+		saturation.set_value_no_signal(last_hsv_watcher.hsv_shift[1])
+		value.set_value_no_signal(last_hsv_watcher.hsv_shift[2])
+		strength.set_value_no_signal(last_hsv_watcher.strength)
+		alpha.set_value_no_signal(last_hsv_watcher.alpha)
