6cd7d4e966ae3f6a57dacc69f87967dd1bd8eec3
enderprism <endxrprism@gmail.com>
2025-10-28 18:32:07 +0100

    fix: multiline string property doesn't emit `value_changed` and can't be unfocused with keyboard

--- src/gui/properties/types/MultilineStringProperty.gd
+++ src/gui/properties/types/MultilineStringProperty.gd
@@ -15,12 +15,19 @@ var input: TextEdit
 func _ready() -> void:
 	label = NodeUtils.get_node_or_add(self, "Label", Label, NodeUtils.INTERNAL)
 	input = NodeUtils.get_node_or_add(self, "Input", TextEdit, NodeUtils.INTERNAL)
-	input.text_changed.connect(func(new_value): value_changed.emit(new_value))
+	input.text_changed.connect(func(): value_changed.emit(input.get_text()))
+	input.focus_entered.connect(func(): Editor.shortcut_blocker = input)
+	input.focus_exited.connect(func(): Editor.shortcut_blocker = null)
 	renamed.connect(refresh)
 	refresh()
 	NodeUtils \
 		.get_node_or_add(self, "PropertyReset", PropertyReset, NodeUtils.INTERNAL) \
 		.set_input(input)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"ui_cancel") and Editor.shortcut_blocker == input:
+		input.release_focus.call_deferred()
 
 
 func set_value(new_value: String) -> void:
@@ -48,6 +55,7 @@ func refresh() -> void:
 	input.placeholder_text = placeholder
 	input.scroll_smooth = true
 	input.scroll_fit_content_height = true
+	input.custom_minimum_size.y = 3 * input.get_line_height() + 2 * input.get_theme_constant(&"line_spacing")
 	if Engine.is_editor_hint():
 		reset()
 
