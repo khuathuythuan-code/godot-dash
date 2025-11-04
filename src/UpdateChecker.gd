024e8dca14da72b400bb96f942f004a05220f0ea
PullSL <paul@anthonyfok.org>
2025-11-03 17:28:10 -0700

    show new/current version in update dialogue

--- src/UpdateChecker.gd
+++ src/UpdateChecker.gd
@@ -11,14 +11,19 @@ func _ready() -> void:
 
 
 func _on_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray):
-	if body.get_string_from_utf8().containsn('config/version="' + ProjectSettings.get_setting("application/config/version") + '"'):
-		text = "Up to date."
+	if body.get_string_from_utf8().containsn('config/version="%s"' % ProjectSettings.get_setting("application/config/version")):
+		text = "Up to date (version %s)." % ProjectSettings.get_setting("application/config/version")
 		await get_tree().create_timer(1.0).timeout
 		var tween := create_tween().tween_property(self, "modulate:a", 0, 1).set_trans(Tween.TRANS_QUINT)
 		await tween.finished
 		hide()
 		return
-	text = "New update available! Click to download."
+	var new_version: String
+	for line in body.get_string_from_utf8().split("\n"):
+		if line.begins_with("config/version="):
+			new_version = line.replacen("config/version=", "").remove_chars('"')
+			break
+	text = "New update available (version %s)! Click to download." %  new_version
 	out_of_date = true
 
 
