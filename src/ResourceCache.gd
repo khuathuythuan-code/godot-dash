1ef8767d9ef3b62157806c7be953663705727d4a
enderprism <endxrprism@gmail.com>
2025-11-06 19:15:34 +0100

    missing type hint

--- src/ResourceCache.gd
+++ src/ResourceCache.gd
@@ -7,6 +7,6 @@ var cache: Dictionary[String, Resource]
 func get_or_load(path: String) -> Resource:
 	if path in cache.keys():
 		return cache[path]
-	var resource = load(path)
+	var resource: Resource = load(path)
 	cache[path] = resource
 	return resource
