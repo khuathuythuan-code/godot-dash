a677f855ae1567698ed478f04222bf93a7aff22d
enderprism <endxrprism@gmail.com>
2025-11-09 12:30:47 +0100

    style guide

--- src/editor/BaseDetailHandler.gd
+++ src/editor/BaseDetailHandler.gd
@@ -11,48 +11,6 @@ func _ready() -> void:
 	detail.input.focus_entered.connect(_on_property_focus_entered)
 
 
-func _on_property_focus_entered() -> void:
-	if color_channel_editor.button_group.get_pressed_button():
-		color_channel_editor.button_group.get_pressed_button().set_pressed(false)
-
-
-func _on_base_color_value_changed(base_channel: String) -> void:
-	var existing_color_channels := LevelManager.current_level.color_channels.map(func(channel): return channel.associated_group.trim_prefix(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX))
-	if base_channel != "" and not base_channel in existing_color_channels:
-		base.set_value_no_signal("")
-		return
-	var objects_base: Array[Node2D]
-	objects_base.assign($"../EditHandler".selection.map(into_base).map(use_hsv_watcher))
-	clear_color_channels(objects_base)
-	if base_channel == "":
-		objects_base.map(_reset_color)
-		return
-	objects_base.map(func(object): object.add_to_group(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + base_channel, true))
-	var watcher: ColorChannelWatcher = get_tree().get_first_node_in_group(ColorChannelWatcher.WATCHER_GROUP_PREFIX + ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + base_channel)
-	watcher.refresh_objects_color(objects_base)
-
-
-func _on_detail_color_value_changed(value:Variant) -> void:
-	var detail_channel := value as String
-	var existing_color_channels := LevelManager.current_level.color_channels.map(func(channel): return channel.associated_group.trim_prefix(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX))
-	if not detail_channel in existing_color_channels:
-		detail.set_value_no_signal("")
-		return
-	var objects_detail: Array = (
-		$"../EditHandler".selection
-			.map(func(object): return object.get_node_or_null(^"Detail") as Node2D)
-			.filter(ArrayUtils.flatten)
-			.map(use_hsv_watcher)
-	)
-	clear_color_channels(objects_detail)
-	if detail_channel == "":
-		objects_detail.map(_reset_color)
-		return
-	objects_detail.map(func(object): object.add_to_group(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + detail_channel, true))
-	var watcher: ColorChannelWatcher = get_tree().get_first_node_in_group(ColorChannelWatcher.WATCHER_GROUP_PREFIX + ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + detail_channel)
-	watcher.refresh_objects_color(objects_detail)
-	
-
 func clear_color_channels(selection: Array) -> void:
 	for color_channel in LevelManager.current_level.color_channels:
 		selection.map(func(object): object.remove_from_group(color_channel.associated_group))
@@ -103,9 +61,49 @@ func _on_edit_handler_selection_changed(selection: Array[Node2D]) -> void:
 	)
 
 
-func _reset_color(object: Node) -> void:
-	object = use_hsv_watcher(object)
-	object.modulate = Color.WHITE
+func _on_property_focus_entered() -> void:
+	if color_channel_editor.button_group.get_pressed_button():
+		color_channel_editor.button_group.get_pressed_button().set_pressed(false)
+
+
+func _on_base_color_value_changed(base_channel: String) -> void:
+	var existing_color_channels := LevelManager.current_level.color_channels.map(func(channel): return channel.associated_group.trim_prefix(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX))
+	if base_channel != "" and not base_channel in existing_color_channels:
+		base.set_value_no_signal("")
+		return
+	var objects_base: Array[Node2D]
+	objects_base.assign($"../EditHandler".selection.map(into_base).map(use_hsv_watcher))
+	clear_color_channels(objects_base)
+	if base_channel == "":
+		objects_base.map(reset_color)
+		return
+	objects_base.map(func(object): object.add_to_group(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + base_channel, true))
+	var watcher: ColorChannelWatcher = get_tree().get_first_node_in_group(ColorChannelWatcher.WATCHER_GROUP_PREFIX + ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + base_channel)
+	watcher.refresh_objects_color(objects_base)
+
+
+func _on_detail_color_value_changed(detail_channel: String) -> void:
+	var existing_color_channels := LevelManager.current_level.color_channels.map(func(channel): return channel.associated_group.trim_prefix(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX))
+	if not detail_channel in existing_color_channels:
+		detail.set_value_no_signal("")
+		return
+	var objects_detail: Array = (
+		$"../EditHandler".selection
+			.map(func(object): return object.get_node_or_null(^"Detail") as Node2D)
+			.filter(ArrayUtils.flatten)
+			.map(use_hsv_watcher)
+	)
+	clear_color_channels(objects_detail)
+	if detail_channel == "":
+		objects_detail.map(reset_color)
+		return
+	objects_detail.map(func(object): object.add_to_group(ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + detail_channel, true))
+	var watcher: ColorChannelWatcher = get_tree().get_first_node_in_group(ColorChannelWatcher.WATCHER_GROUP_PREFIX + ColorChannelItem.COLOR_CHANNEL_GROUP_PREFIX + detail_channel)
+	watcher.refresh_objects_color(objects_detail)
+	
+
+static func reset_color(hsv_watcher: HSVWatcher) -> void:
+	hsv_watcher.modulate = Color.WHITE
 
 
 static func use_hsv_watcher(object: Node) -> HSVWatcher:
