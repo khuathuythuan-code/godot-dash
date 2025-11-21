bcfd93f548ebf787b3f10b2ef753a444281d129f
PushSL <peter@anthonyfok.org>
2025-11-20 17:42:44 -0700

    ditch culling for ninepatchsprite

--- src/NinePatchSprite2DAbsoluteSize.gd
+++ src/NinePatchSprite2DAbsoluteSize.gd
@@ -6,11 +6,9 @@ class_name NinePatchSprite2DAbsoluteSize
 
 
 func _process(_delta: float) -> void:
-	if NodeUtils.is_on_screen(parent, Constants.Axis.X):
-		_on_size_changed()
+	_on_size_changed()
 
 
 func _on_size_changed() -> void:
-	print("cahnged")
 	nine_patch_sprite.global_scale = Vector2.ONE/4
 	nine_patch_sprite.size = abs(parent.global_scale) * 512
