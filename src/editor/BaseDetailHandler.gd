ccb8b5b5777b6b44df47ec6152833ef39466dc0f
PullSL <paul@anthonyfok.org>
2025-12-17 17:36:16 -0700

    fix: base and detail color properties are shown on player

--- src/editor/BaseDetailHandler.gd
+++ src/editor/BaseDetailHandler.gd
@@ -48,7 +48,7 @@ func _load_detail(objects_detail: Array[HSVWatcher]) -> void:
 
 
 func _on_edit_handler_selection_changed(selection: Selection) -> void:
-	if selection.is_empty():
+	if selection.is_empty() or (selection.size() == 1 and selection.first() is Player):
 		return
 	# Base
 	var objects_base: Array[HSVWatcher]
