b1338d5732d1e8dd2d68a51ca1162743434a58f3
enderprism <endxrprism@gmail.com>
2025-12-25 15:52:30 +0100

    switch trail rendering to process instead of physics process

--- addons/trail_2d/trail_2d.gd
+++ addons/trail_2d/trail_2d.gd
@@ -1,21 +1,21 @@
 extends Line2D
 
 @export_category('Trail')
-@export var length : = 10
+@export var length: int = 10
 
-@onready var parent : Node2D = get_parent()
-var offset : = Vector2.ZERO
+@onready var parent: Node2D = get_parent()
+var offset := Vector2.ZERO
+
 
 func _ready() -> void:
 	offset = position
 	top_level = true
 
-func _physics_process(_delta: float) -> void:
-	global_position = Vector2.ZERO
 
-	var point : = parent.global_position + offset
+func _process(_delta: float) -> void:
+	global_position = Vector2.ZERO
+	var point: Vector2 = parent.global_position + offset
 	if points.is_empty() or point != points[0]:
 		add_point(point, 0)
-	
 	if get_point_count() > length / abs(Engine.time_scale):
 		remove_point(get_point_count() - 1)
