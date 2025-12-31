447774607b1092cc82c2337149fa3bde678fda9d
enderprism <endxrprism@gmail.com>
2025-12-31 12:01:03 +0100

    fix: player becomes invalid in selections

--- src/refcounted/PathRef.gd
+++ src/refcounted/PathRef.gd
@@ -10,11 +10,17 @@ func _init(node: Node) -> void:
 		_ref.tree_entered.connect(func(): _path = Editor.root.level.get_path_to(_ref))
 	else:
 		_path = Editor.root.level.get_path_to(node)
-	_ref.renamed.connect(func(): _path = Editor.root.level.get_path_to(_ref))
+	_ref.renamed.connect(_update_path)
 
 
 func to_ref() -> Node:
 	if not _ref:
 		_ref = Editor.root.level.get_node_or_null(_path)
-		_ref.renamed.connect(func(): _path = Editor.root.level.get_path_to(_ref))
+		_ref.renamed.connect(_update_path)
 	return _ref
+
+
+func _update_path() -> void:
+	if "__freed" in _ref.name:
+		return
+	_path = Editor.root.level.get_path_to(_ref)
