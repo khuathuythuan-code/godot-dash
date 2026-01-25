b0d0210b9f2d52d7911d46ce3ef19baaa51e7b51
PullSL <paul@anthonyfok.org>
2026-01-24 20:48:33 -0700

    fix: modulation isn't set after playtest stops

--- src/EditorScene.gd
+++ src/EditorScene.gd
@@ -185,6 +185,8 @@ func stop_playtest() -> void:
 	practice_button.set_pressed_no_signal(false)
 	$GameScene/PauseMenuLayer/PauseMenu.get_node(^"%Restart").hide()
 	%Playtest.disabled = false
+	await get_tree().process_frame
+	Editor.render_mode_manager.update_hsvwatcher.emit()
 
 
 func _fade_leave(_action: Variant = null) -> void:
