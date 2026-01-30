0efac379b88e8f52cbe8afb2f7f0e1e54059709c
PullSL <paul@anthonyfok.org>
2026-01-29 21:34:40 -0700

    fix: object mode doesn't disable enter effects the first time it's selected

--- src/RenderModes.gd
+++ src/RenderModes.gd
@@ -35,8 +35,8 @@ func enable_object_mode() -> void:
 	level.ground_color = Color.GRAY
 	level.background_color = Color.GRAY
 	level.line_color = Color.WHITE
-	level.enter_effect = level.enter_effect # Trigger setter
 	mode = Mode.OBJECT_MODE # Locks editing of level colors
+	level.enter_effect = level.enter_effect # Trigger setter
 	LevelManager.update_hsv_watchers.emit()
 
 
