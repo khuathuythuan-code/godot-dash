1726e9f3ae263b947de1553c282914fc57fe128e
enderprism <endxrprism@gmail.com>
2026-02-26 22:58:26 +0100

    avoid crash with blue dash orb

--- src/interactables/private_components/FireDashPathFollowSpeed.gd
+++ src/interactables/private_components/FireDashPathFollowSpeed.gd
@@ -1,6 +1,12 @@
 extends Node
+
 class_name PathFollowSpeed
 
 func _ready() -> void:
 	var parent := get_parent() as FireDashComponent
 	parent.path = self
+
+
+func get_velocity(player: Player) -> Vector2:
+	print_debug("TODO: implement PathFollowSpeed")
+	return Vector2.ZERO
