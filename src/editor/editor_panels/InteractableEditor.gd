d5efcb71e495ccee4e1440d4a051687e1f27f49b
enderprism <endxrprism@gmail.com>
2026-03-03 12:00:31 +0100

    feat: inspector generated buttons: support icons

--- src/editor/editor_panels/InteractableEditor.gd
+++ src/editor/editor_panels/InteractableEditor.gd
@@ -118,7 +118,10 @@ func build_ui(interactables: Selection) -> void:
 				elif field.hint == PROPERTY_HINT_TOOL_BUTTON:
 					var call_button: Button = Button.new()
 					call_button.text = field.hint_string.get_slice(",", 0)
+					call_button.icon = load("res://assets/textures/godot_editor_icons/%s.svg" % field.hint_string.get_slice(",", 1))
+					call_button.expand_icon = true
 					call_button.pressed.connect(component.get(field_name) as Callable)
+					call_button.custom_minimum_size.y = 34.0
 					if last_section:
 						var section_vboxcontainer := last_section.get_child(0) as VBoxContainer
 						section_vboxcontainer.add_child(call_button)
