8d4ee41e7dfc093ec54a52f4fcc5cd8d8ad2f152
PullSL <paul@anthonyfok.org>
2026-03-06 16:29:37 -0700

    feat: physics properties saving, loading, and editing

---
+++ src/editor/editor_panels/PhysicsEditor.gd
@@ -0,0 +1,108 @@
+extends VBoxContainer
+
+class_name PhysicsEditor
+
+@onready var parent: Node = get_parent()
+
+@export var physics_object_property: BoolProperty
+@export var mass_property: FloatProperty
+@export var friction_property: FloatProperty
+@export var rough_property: BoolProperty
+@export var bounce_property: FloatProperty
+@export var absorbent_property: BoolProperty
+@export var gravity_scale_property: FloatProperty
+
+var solid_objects: Selection = Selection.new()
+
+
+func _on_edit_handler_selection_changed(_selection: Selection) -> void:
+	solid_objects = _selection.filter(func(object): return object is StaticBody2D)
+	if solid_objects.is_empty():
+		get_parent().hide()
+		return
+	get_parent().show()
+	var data: Dictionary = solid_objects.first().get_meta("physics")
+	physics_object_property.set_value_no_signal(data.physics_object)
+	mass_property.set_value_no_signal(data.mass)
+	friction_property.set_value_no_signal(data.friction)
+	rough_property.set_value_no_signal(data.rough)
+	bounce_property.set_value_no_signal(data.bounce)
+	absorbent_property.set_value_no_signal(data.absorbent)
+	gravity_scale_property.set_value_no_signal(data.gravity_scale)
+
+
+func _on_physics_object_value_changed(value: bool) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.physics_object = value
+			component.update_meta()
+	)
+
+
+func _on_mass_value_changed(value: float) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.mass = value
+			component.update_meta()
+	)
+
+
+func _on_friction_value_changed(value: float) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.friction = value
+			component.update_meta()
+	)
+
+
+func _on_rough_value_changed(value: bool) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.rough = value
+			component.update_meta()
+	)
+
+
+func _on_bounce_value_changed(value: float) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.bounce = value
+			component.update_meta()
+	)
+
+
+func _on_absorbent_value_changed(value: bool) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.absorbent = value
+			component.update_meta()
+	)
+
+
+func _on_gravity_scale_value_changed(value: float) -> void:
+	if solid_objects.is_empty():
+		return
+	solid_objects.for_each(
+		func(object: Object):
+			var component: PhysicsObjectComponent = NodeUtils.get_child_of_type(object, PhysicsObjectComponent)
+			component.gravity_scale = value
+			component.update_meta()
+	)
--- src/interactables/public_components/PhysicsObjectComponent.gd
+++ src/interactables/public_components/PhysicsObjectComponent.gd
@@ -10,14 +10,32 @@ class_name PhysicsObjectComponent
 @export var absorbent: bool = false
 @export var gravity_scale: float = 1.0
 
+
 func _ready() -> void:
 	LevelManager.level_started.connect(_start)
+	update_meta()
+
+
+func update_meta() -> void:
+	var data: Dictionary = {
+		"physics_object": physics_object,
+		"mass": mass,
+		"friction": friction,
+		"rough": rough,
+		"bounce": bounce,
+		"absorbent": absorbent,
+		"gravity_scale": gravity_scale,
+	}
+	get_parent().set_meta("physics", data)
 
 
 func _start() -> void:
 	if not physics_object or get_parent() is RigidBody2D:
 		return
 	var previous_parent: StaticBody2D = get_parent()
+	for child in NodeUtils.get_children_of_type(previous_parent, Node2D):
+		child.scale *= previous_parent.scale
+	NodeUtils.get_child_of_type(previous_parent, NinePatchSprite2DAbsoluteSize).scale = previous_parent.scale
 	var new_parent: RigidBody2D = RigidBody2D.new()
 	new_parent.transform = previous_parent.transform
 	new_parent.collision_mask = 2103
@@ -33,3 +51,14 @@ func _start() -> void:
 	var nine_patch_sprite_2d_absolute_size: NinePatchSprite2DAbsoluteSize = NodeUtils.get_child_of_type(new_parent, NinePatchSprite2DAbsoluteSize)
 	if nine_patch_sprite_2d_absolute_size:
 		nine_patch_sprite_2d_absolute_size.parent = new_parent
+
+
+func use_data(data: Dictionary) -> void:
+	physics_object = data.physics_object
+	mass = data.mass
+	friction = data.friction
+	rough = data.rough
+	bounce = data.bounce
+	absorbent = data.absorbent
+	gravity_scale = data.gravity_scale
+	update_meta()
