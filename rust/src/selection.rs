60d1fefe1f69c1128c01c00f21cf9d87052932d0
enderprism <endxrprism@gmail.com>
2026-03-28 00:09:49 +0100

    fix(rust): use typed dict

--- rust/src/selection.rs
+++ rust/src/selection.rs
@@ -211,8 +211,7 @@ impl Selection {
     #[func]
     /// Like [method Selection.map_generic], but it produces a [Dictionary] with,
     /// for each element, keys and values being `element` and `method.call(element)`.
-    fn map_generic_dict(&self, method: Callable) -> VarDictionary {
-        // Typed dictionaries aren't supported in godot-rust yet
+    fn map_generic_dict(&self, method: Callable) -> Dictionary<Gd<Node2D>, Variant> {
         self.inner
             .clone()
             .into_iter()
