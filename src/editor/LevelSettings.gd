bb7086817492d9185077a2d37fffe12effa5c250
enderprism <endxrprism@gmail.com>
2026-03-28 12:00:54 +0100

    feat(levelsettings): preview song

--- src/editor/LevelSettings.gd
+++ src/editor/LevelSettings.gd
@@ -1,7 +1,12 @@
 class_name LevelSettings
 extends Control
 
+@export var song_path: FileProperty
+@export var song_start_offset: FloatProperty
+@export var preview_button: Button
+
 var saveloads: Array[PropertySaveLoad]
+var is_previewing: bool = false
 
 
 func _ready() -> void:
@@ -21,3 +26,22 @@ func refresh_saveloads(level: Level) -> void:
 func _on_close_pressed() -> void:
 	Editor.shortcut_blocker = null
 	hide()
+
+
+func _on_preview_pressed() -> void:
+	if song_path.get_value().is_empty():
+		is_previewing = false
+		preview_button.icon = load("res://assets/textures/icons/godot/Play.svg")
+		LevelManager.level_song_player.stop()
+		LevelManager.level_song_player.stream = AssetManager.load_song_threaded_get(LevelManager.current_level.song_path)
+		return
+
+	is_previewing = not is_previewing
+	if is_previewing:
+		preview_button.icon = load("res://assets/textures/icons/godot/Stop.svg")
+		LevelManager.level_song_player.stream = AssetManager.load_song(song_path.get_value())
+		LevelManager.level_song_player.play(song_start_offset.get_value())
+	else:
+		preview_button.icon = load("res://assets/textures/icons/godot/Play.svg")
+		LevelManager.level_song_player.stop()
+		LevelManager.level_song_player.stream = AssetManager.load_song_threaded_get(LevelManager.current_level.song_path)
