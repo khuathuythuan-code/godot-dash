4ba8ddefeb5e24da0a0b9e0c98d53fd0062f0271
enderprism <endxrprism@gmail.com>
2026-04-05 14:07:35 +0200

    initial impl

--- src/editor/InspectorTree.gd
+++ src/editor/InspectorTree.gd
@@ -2,4 +2,12 @@ class_name InspectorTree
 extends Tree
 
 func refresh() -> void:
-	pass
+	clear()
+	var layers: Array[Layer] = Editor.root.level.layers
+	var root: TreeItem = create_item()
+	for layer: Layer in layers:
+		var layer_item: TreeItem = create_item(root)
+		layer_item.set_text(0, layer.name)
+		for object: Node2D in layer.get_children():
+			var object_item: TreeItem = create_item(layer_item)
+			object_item.set_text(0, object.name)
