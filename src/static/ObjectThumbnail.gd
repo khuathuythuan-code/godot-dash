d12eecf61ea4492813459f9474fadb524161fa21
enderprism <endxrprism@gmail.com>
2026-04-08 17:45:56 +0200

    fix: bricks and rebound pad are missing a tree thumbnail

--- src/static/ObjectThumbnail.gd
+++ src/static/ObjectThumbnail.gd
@@ -19,7 +19,7 @@ static func get_object_thumbnail_image(object: Node2D) -> Array[Image]:
 			images.append(child.get_composite_image())
 		elif (child is Sprite2D or child is NinePatchSprite2D) and child.texture:
 			images.append(child.texture.get_image())
-		elif child is CanvasGroup:
+		elif child is CanvasGroup or child is Polygon2D:
 			images.append_array(get_object_thumbnail_image(child))
 	return images
 
@@ -44,12 +44,12 @@ static func fit_size_to_square(size: Vector2i, side_length: int) -> Vector2i:
 
 ## Generate a square thumbnail of the object.
 static func generate(object: Node2D, side_length: int) -> ImageTexture:
+	var composite_image_size: Vector2i = Vector2i.ONE * side_length
 	var images: Array[Image] = get_object_thumbnail_image(object)
 	for image: Image in images:
 		var image_size: Vector2i = fit_size_to_square(image.get_size(), side_length)
 		image.resize(image_size.x, image_size.y, Image.Interpolation.INTERPOLATE_LANCZOS)
-	var composite_image: Image = Image.create_empty(side_length, side_length, false, Image.FORMAT_RGBA8)
-	var composite_image_size: Vector2i = Vector2i.ONE * side_length
+	var composite_image: Image = Image.create_empty(side_length, side_length, false, images[0].get_format())
 	for image: Image in images:
 		var image_rect: Rect2i = Rect2i(Vector2i.ZERO, image.get_size())
 		composite_image.blend_rect(image, image_rect, (composite_image_size - image.get_size()) / 2.0)
