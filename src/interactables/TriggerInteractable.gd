c4e90dcfeb4f9f93f30406a3265cb072c8bc6e76
khuathuythuan-code <khuathuythuan@gmail.com>
2026-04-22 10:34:36 +0700

    sửa lại end level: vị trí hút người chơi, và xác nhận lại lỗi người chơi bị hút vào trigger chưa đc xác định, ko phải do tạo lại scene

--- src/interactables/TriggerInteractable.gd
+++ src/interactables/TriggerInteractable.gd
@@ -3,18 +3,12 @@ extends Interactable
 
 func _ready() -> void:
 	super()
-	#khi player đi vào endLeveltrigger ván 1 -> hủy ván, nhưng signal body_enter chưa kịp hủy trong engine 
-	#-> đã render lại map -> tuy player chưa chạm vào endLeveltrigger ván 2 tạo nhưng vẫn bị hút vào
-	# có lẽ logic chưa kịp điều chỉnh do frame hủy và frame tạo mới quá nhanh
-#1. Transform node ≠ Physics body
-#Khi set player.global_position = (640), node di chuyển ngay lập tức nhưng physics body cần ít nhất 1 physics frame để sync theo. Trong khoảng thời gian đó, collision detection vẫn dùng vị trí cũ.
-#2. Area2D mới tạo ra sẽ emit body_entered cho tất cả body đang overlap tại thời điểm physics frame đầu tiên
-#Không quan trọng body có "đi vào" hay không — chỉ cần tại physics frame đầu tiên physics body còn ở vị trí cũ overlap với Area2D mới → body_entered emit ngay.
+	#đoạn code này
 	monitoring = false
 	await get_tree().process_frame
 	await get_tree().process_frame
 	monitoring = true
-	#nên disable area2d mới 2 frame để tránh nhận signal từ area2d cũ
+	#chữa lỗi hút vào trigger
 	body_entered.connect(func(player: Player): 
 		interacted.emit(player)
 		
--- src/interactables/public_components/EndLevelComponent.gd
+++ src/interactables/public_components/EndLevelComponent.gd
@@ -48,13 +48,17 @@ func _on_static_easing_progressed(player: Player, weight_delta: float) -> void:
 	var target: Node2D = parent.query(TargetObjectComponent).target_to_node()
 	var weight: float = static_trigger.query(EasingComponent).weights[player]
 	var rotation_direction = 1 if player.global_position < target.global_position else -1
-	player.global_position = initial_player_position.lerp(target.global_position, weight)
+	player.global_position = initial_player_position.lerp(initial_player_position+Vector2(0,-800), weight)
 	player.rotation += weight_delta * 5 * rotation_direction
-
+	LevelManager.player_camera.static_factor = Vector2.ZERO
+	pass
 
 func _on_static_easing_finished(player: Player) -> void:
 	shake_trigger.interacted.emit(player)
 	player.hide()
+	get_parent().visible = true
+
+	pass
 
 
 func _on_shake_easing_finished(_player: Player) -> void:
