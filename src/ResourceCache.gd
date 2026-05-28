fde5b4ec62d663e9d0cfa4781490d82b83957f5c
khuathuythuan-code <khuathuythuan@gmail.com>
2026-05-28 10:40:47 +0700

    fix: validate cho lỗi texture_override.base đang là "assets/textures" thay vì đầy đủ như "assets/textures/solids/regular_blocks/RegularBlock01.svg"

--- src/ResourceCache.gd
+++ src/ResourceCache.gd
@@ -7,6 +7,8 @@ var cache: Dictionary[String, Resource]
 func get_or_load(path: String) -> Resource:
 	if path in cache.keys():
 		return cache[path]
+	if not ResourceLoader.exists(path):
+		return null
 	var resource: Resource = load(path)
 	cache[path] = resource
 	return resource
