e455beabb2066140e791a3d20eb6cdaee0b61306
khuathuythuan-code <khuathuythuan@gmail.com>
2026-08-15 15:24:58 +0700

    chore: gán bus tương ứng cho các sound effect để khi chỉnh bus tương ứng thì sẽ giảm theo

--- src/autoloads/SFXManager.gd
+++ src/autoloads/SFXManager.gd
@@ -1,9 +1,10 @@
 extends Node
 
-func play_sfx(sfx_path: String) -> void:
+func play_sfx(sfx_path: String, bus_name: StringName = &"Game SFX") -> void:
 	var sfx_player = AudioStreamPlayer.new()
 	add_child(sfx_player)
 	sfx_player.stream = load(sfx_path)
+	sfx_player.set_bus(bus_name)
 	sfx_player.play()
 	await sfx_player.finished
 	sfx_player.queue_free()
